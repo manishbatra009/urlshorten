8d55321abaa1c438e4e3c55921822b5d3d85631e
Manish Batra <manishbatra@ciqindia.com>
2018-03-18 19:30:19 +0530

    test commit

--- fetch/models.py
+++ fetch/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+class urls(models.Model):
+    longURL = models.TextField(blank=False)
+    shortURL = models.CharField(max_length=8)
+    count = models.IntegerField()
+
+    def __str__(self):
+        return "urls: ".format(self.shortURL)
+    
 # Create your models here.
