2ce0f4b47d3a4464d519710e1defd3285130469a
manishbatra <batra.manish4@gmail.com>
2018-03-19 00:35:17 +0530

    long_url short_url

--- fetch/models.py
+++ fetch/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class urls(models.Model):
+class URLS(models.Model):
     longURL = models.TextField(blank=False)
     shortURL = models.CharField(max_length=8)
     count = models.IntegerField()
--- fetch/views.py
+++ fetch/views.py
@@ -3,25 +3,72 @@ from django.http import HttpResponse
 from . import CsrfExemptSessionAuthentication
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from django.views.decorators.csrf import csrf_exempt
+import json
+from .models import URLS
+from django.http import JsonResponse
+from django.core.validators import URLValidator
+from django.core.exceptions import ValidationError
+
 
 
 #authentication_classes = (CsrfExemptSessionAuthentication, BasicAuthentication)
+def url_shortify(longurl):
+    return
 
+def url_verify(longurl):
+    val = URLValidator(verify_exists=False)
+    if val(longurl):
+        return True
+    else:
+        return False
 
 @csrf_exempt
 def short_url(request):
-    if request.method == 'POST':
-        return HttpResponse(request.body)
-def short_urls(request):
-    return HttpResponse("2")
+
+    longurl = json.loads(request.body)[long_url]
+    if url_verify(longurl):
+        if URLS.objects.get(longURL=longurl):
+            u = URLS.objects.get(longURL=longurl)
+            return JsonResponse({'short_url': u.shortURL})
+        else:
+            shorturl = url_shortify(longurl)
+            u = URLS()
+            u.longURL=longurl
+            u.shortURL=shorturl
+            return JsonResponse({'short_url': shorturl})
+    else:
+        return HttpResponse("error")
+
+
+@csrf_exempt
 def long_url(request):
+
+    shorturl = json.loads(request.body)[short_url]
+    if URLS.objects.get(shortURL = shorturl):
+        u = URLS.objects.get(shortURL = shorturl)
+        return JsonResponse({'long_url': u.longURL})
+    else:
+        return HttpResponse("error")
+
+
+
+@csrf_exempt
+def short_urls(request):
+    longurls = json.loads(request.body)[long_url]
     return HttpResponse("3")
+
+@csrf_exempt
 def long_urls(request):
     return HttpResponse("4")
+
 def count(request):
-    return HttpResponse("5")
-def verify(request):
-    return HttpResponse("6")
+    shorturl = json.loads(request.body)[short_url]
+    if URLS.objects.get(shortURL=shorturl):
+        u = URLS.objects.get(shortURL=shorturl)
+        return JsonResponse({'count':u.count})
+    else:
+        return HttpResponse("error")
+
 
 def index(request):
     return HttpResponse("0")
